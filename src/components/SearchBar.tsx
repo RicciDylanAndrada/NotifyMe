915ab50ab2b1ef114835e3594669e5bf763bc41e
Dylan Andrada <dylanandrada@Dylans-MacBook-Pro.local>
2025-01-23 20:39:51 -0500

    add searchbar

--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-
 export const SearchBar = () => {
   const handleSubmit = () => {};
-    return (
-      <form
-        className="flex flex-wrap gap-4 mt-12"
-        onSubmit={handleSubmit}
-        >
-            
-            
-      </form>
-    );
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+      <button type="submit" className="searchbar-btn">
+        Search
+      </button>
+    </form>
+  );
 };
